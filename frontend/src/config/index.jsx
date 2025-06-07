30df42beaa5fdb2b72b5bfd12808830bc3405d5d
asthas0108 <asthas0108@gmail.com>
2025-06-07 11:29:24 +0530

    added server

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const server = "http://localhost:5000";
+// export const server = "http://localhost:5000";
+export const server ="https://elevateu-1bbk.onrender.com";
 
 export const clientServer = axios.create({
     baseURL: server,
